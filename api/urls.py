38856aca0f49dec27ad3d459357296674346d118
kibaara-wangu <kibaarawangu@gmail.com>
2024-08-13 22:00:17 +0300

    added files

--- api/urls.py
+++ api/urls.py
@@ -13,35 +13,15 @@ from .views import TeacherDetailView
 
 
 urlpatterns = [
-    path(
-        "students/",StudentListView.as_view(),name="student_list_view"
-    ),
-    path(
-        "classes/", ClassListView.as_view(), name="class_list_view"
-    ),
-    path(
-        "periods/", ClassroomPeriodListView.as_view(), name="classroomPeriod_list_view"
-    ),
-    path(
-        "courses/", CoursesListView.as_view(), name="course_list_view" 
-    ),
-    path(
-        "teachers/", TeacherListView.as_view(), name="teachers_list_view"
-    ),
-     path(
-        "students/<int:id>/", StudentDetailView.as_view(), name="student_detail_view"
-    ),
-      path(
-        "classes/<int:id>/", ClassDetailView.as_view(), name="class_detail_view"
-    ),
-     path(
-        "classroom_period/<int:id>/", ClassroomPeriodDetailView.as_view(), name="classroom_periods_detail_view"
-    ),
-     path(
-        "courses/<int:id>/", CoursesDetailView.as_view(), name="courses_detail_view"
-    ), 
-     path(
-        "teachers/<int:id>/", TeacherDetailView.as_view(), name="teachers_detail_view"
-    )
+    path("students/",StudentListView.as_view(),name="student_list_view" ),
+    path("classes/", ClassListView.as_view(), name="class_list_view"),
+    path("periods/", ClassroomPeriodListView.as_view(), name="classroomPeriod_list_view"),
+    path("courses/", CoursesListView.as_view(), name="course_list_view" ),
+    path("teachers/", TeacherListView.as_view(), name="teachers_list_view"),
+    path("students/<int:id>/", StudentDetailView.as_view(), name="student_detail_view"),
+    path("classes/<int:id>/", ClassDetailView.as_view(), name="class_detail_view"),
+    path("classroom_period/<int:id>/", ClassroomPeriodDetailView.as_view(), name="classroom_periods_detail_view"),
+    path("courses/<int:id>/", CoursesDetailView.as_view(), name="courses_detail_view"), 
+    path("teachers/<int:id>/", TeacherDetailView.as_view(), name="teachers_detail_view")
 ]
 
